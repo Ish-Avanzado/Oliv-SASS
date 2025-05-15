e7916a3d66ee3521d18cbcf5de29c6974e598111
Kenken-ladlad <kenkenladlad@gmail.com>
2025-05-15 15:40:47 +0800

    updating files

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,22 @@
 const burger = document.querySelector(".toggle_menu");
 const hNav = document.querySelector(".header__nav");
 burger.addEventListener("click", () => {
-    burger.classList.toggle("open");
-    hNav.classList.toggle("open");
-})
+  burger.classList.toggle("open");
+  hNav.classList.toggle("open");
+});
+
+const aHeader = document.querySelectorAll(".accordion__header");
+aHeader.forEach((Header) => {
+  Header.addEventListener("click", () => {
+    // Header.querySelector(".accordion__header").classList.toggle("active");
+    Header.nextElementSibling.classList.toggle("active");
+  });
+});
+
+// const headers = document.querySelectorAll(".accordion__header");
+// headers.forEach((header) => {
+//     header.addEventListener('click', () => {
+//         header.nextElementSibling.classList.toggle("active");
+        
+//     });
+// });
